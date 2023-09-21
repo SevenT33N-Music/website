53fd0a4b00d01e1101a2d89e1c269cea4342ca17
SevenT33N-Music <135186375+SevenT33N-Music@users.noreply.github.com>
2023-09-21 15:40:16 +0000

    Added Packages

--- replit.nix
+++ replit.nix
@@ -1,5 +1,8 @@
 { pkgs }: {
   deps = [
+    pkgs.notmuch-bower
+    pkgs.notmuch-bower
+    pkgs.nodejs-16_x
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server  
   ];
