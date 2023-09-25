0441a8a43acc11e710d8ac083e2b6a4c0099f28d
SevenT33N-Music <135186375+SevenT33N-Music@users.noreply.github.com>
2023-09-25 15:22:02 +0000

    Quality of Life Updates

--- updating.js
+++ updating.js
@@ -1,17 +1,21 @@
 var update = 0; // 0 = not updated, 1 = updated
-var updateReason = 1; // 2 = bug, 1 = new feature, 3 = Editing Code
+var updateReason = 2; // 2 = bug, 1 = new feature, 3 = Editing Code
 var checkUpdateReason = localStorage.getItem("updateReason");
 if (checkUpdateReason == null || checkUpdateReason == "") {
   if (updateReason == 1) {
     localStorage.setItem("updateReason", "1");
   } else if (updateReason == 2) {
     localStorage.setItem("updateReason", "2");
+  } else if (updateReason == 3) {
+    localStorage.setItem("updateReason", "3");
   }
 } else {
   if (updateReason == 1) {
     localStorage.setItem("updateReason", "1");
   } else if (updateReason == 2) {
     localStorage.setItem("updateReason", "2");
+  } else if (updateReason == 3) {
+    localStorage.setItem("updateReason", "3");
   }
 }
 function setCookie(cname, cvalue, exdays) {
@@ -39,7 +43,10 @@ function getCookie(cname) {
 function checkUpdate() {
   let cookieValue = getCookie('adminLog');
   if (update == 1 && cookieValue != '1') {
+    localStorage.setItem("updatingWebiste", "yes");
     window.location.href = "/updating.html";
+  } else if (update == 0) {
+    localStorage.setItem("updatingWebiste", "no");
   }
 }
 checkUpdate();
